84ce4d12bd226bc91d513b2e7798d3b0dfeb5015
Vlad <dr.virtuozov@ya.ru>
2015-11-18 03:36:25 +0000

    second commit

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ var request = require("request");
 
 var Bot = function(TOKEN) {
     this.TOKEN = TOKEN;
-    this.URL = "https://api.telegram.org/bot" + TOKEN;
+    this.URL = "https://api.telegram.org/bot" + TOKEN + "/";
 }
 
 Bot.prototype.sendMessage = function(message, id) {
-    if (id < 0) request.post(this.URL + "sendMessage?chat_id=%s&text=%s", id, message);
-    else request.post(this.URL + "sendMessage?user_id=%s&text=%s", id, message);
+    request.post(this.URL + "sendMessage?chat_id=" + id + "&text=" + message, function(error, response, body){
+        if (response.statusCode == 200) {
+            console.log("Success!");
+        } else {
+            console.log('Error: '+ response.statusCode);
+            console.log(body);
+        }
+    });
 };
 
 module.exports = Bot;
