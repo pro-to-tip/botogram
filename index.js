7a49ab440782d77b334ef3b3f12cd8b86a214e4a
Vlad <dr.virtuozov@ya.ru>
2016-05-15 23:05:44 +0300

    Update index.js
    govnokod is almost gone

--- index.js
+++ index.js
@@ -5,77 +5,86 @@ const EventEmitter = require("events").EventEmitter;
 
 
 class Bot {
-    constructor(TOKEN) {
-        this.TOKEN = TOKEN;
-        this.URL = "https://api.telegram.org/bot" + TOKEN + "/";
-        this.DATA = {};
-        this.emitter = new EventEmitter();
-        
-        this._getMe();
-    }
+  constructor(token) {
+    this.token = token;
+    this.url = "https://api.telegram.org/bot" + token + "/";
+    this.data = {};
+    this._emitter = new EventEmitter();
     
-    sendMessage(message, id) {
-        request.post(encodeURI(this.URL + "sendMessage?chat_id=" + id + "&text=" + message), (err, res, body) => {
-            if (!err && res.statusCode != 200) {
-                throw new Error(res.statusCode);
-            }
-        });
-    }
+    this._types = {
+      message: body => this._messageHandler(body),
+      callback_query: body => this._callbackQueryHandler(body)
+    };
     
-    on(event, callback) {
-        this.emitter.on(event, callback);    
-    }
+    this._getMe();
+  }
+  
+  listen(body) {
+    let type = Object.keys(body)[1];
     
-    listen(body) {
-        var message = body.message;
-        
-        if (message.text) {
-            if (this.emitter.emit("text", message)) {
-                this._logMessage(message);
-            } else {
-                console.log("Bot's ontext listener is not defined.")
-            }
-        } else if (message.sticker) {
-            if (this.emitter.emit("sticker", message)) {
-                this._logMessage(message);
-            } else {
-                console.log("Bot's onsticker listener is not defined.")
-            }
-        } else if (message.photo) {
-            if (this.emitter.emit("photo", message)) {
-                this._logMessage(message);
-            } else {
-                console.log("Bot's onphoto listener is not defined.")
-            }
-        } else if (message.document) {
-            if (this.emitter.emit("document", message)) {
-                this._logMessage(message);
-            } else {
-                console.log("Bot's ondocument listener is not defined.")
-            }
-        } else if (message.voice) {
-            if (this.emitter.emit("voice", message)) {
-                this._logMessage(message);
-            } else {
-                console.log("Bot's onvoice listener is not defined.")
-            }
-        }
+    if (this._types[type]) {
+      this._types[type](body);
+    } else {
+      console.log("Bot Api Error. There is no that message handler:", type);
     }
+  }
+  
+  sendMessage(text, id, params) {
+    return new Promise((resolve, reject) => {
+      params = params || {};
+      params.encode === undefined && params.encode !== false ? params.encode = true : params.encode = false;
+      
+      request.post(this.URL + "sendMessage?chat_id=" + id + "&text=" +
+        (params.encode ? encodeURI(text) : text) +
+        (params.parse_mode ? "&parse_mode=" + params.parse_mode : "") +
+        (params.disable_web_page_preview === true? "&disable_web_page_preview=true" : "") +
+        (params.reply_to_message_id ? "&reply_to_message_id=" + params.reply_to_message_id : "") +
+        (params.reply_markup ? "&reply_markup=" + JSON.stringify(params.reply_markup) : ""), (err, res, body) => {
+          if (err) return reject(err);
+          if (res.statusCode > 400) return reject(res);
+          
+          resolve(JSON.parse(body));
+      });
+    });
+  }
+  
+  on(event, callback) {
+    this._emitter.on(event, callback);    
+  }
+  
+  _messageHandler(body) {
+    let type = Object.keys(body.message)[4];
     
-    _getMe() {
-        request(this.URL + "getMe", (err, res, body) => {
-            if (!err && res.statusCode === 200) {
-                this.DATA = JSON.parse(body).result;
-            } else {
-                throw new Error(err);
-            }
-        });
+    if (this._emitter.emit(type, body.message)) {
+      this._logMessage(body.message);
+    } else {
+      console.log(`Bot's on${type} listener is not defined.`);
     }
+  }
+  
+  _callbackQueryHandler(body) {
+    if (this._emitter.emit("callback_query", body.callback_query)) {
+      this._logMessage(body.callback_query);
+    } else {
+      console.log("Bot's callback_query listener is not defined.");
+    }
+  }
+  
+  _getMe() {
+    request(this.url + "getMe", (err, res, body) => {
+      if (!err && res.statusCode === 200) {
+        this.data = JSON.parse(body).result;
+      } else {
+        throw new Error(err);
+      }
+    });
+  }
+  
+  _logMessage(message) {
+    let type = Object.keys(message)[4];
     
-    _logMessage(message) {
-        console.log("(" + new Date().toUTCString() + ") => %s: [%s] %s %s (%s): %s", 
-            this.DATA.first_name, message.from.username, message.from.first_name, message.from.last_name, message.from.id, message.text);
-    }
+    console.log(`(${new Date().toUTCString()}) => ${this.data.first_name}: [${message.from.username}] ${message.from.first_name} ${message.from.last_name} (${message.from.id}): ${message[type]}`);
+  }
 }
 
 module.exports = Bot;
